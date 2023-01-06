cb2094dc5741f6468e170899a73153aa820d96a9
Vladimir Egorov <vpegorov95@gmail.com>
2023-01-06 10:03:59 +0500

    инициализация проекта

---
+++ src/main/java/com/example/eshop/SimpleEshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleEshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleEshopApplication.class, args);
+    }
+
+}
